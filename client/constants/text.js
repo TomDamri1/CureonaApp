108ac14e026c305aeb89b0034220174ddc2fb3e2
TomDamri1 <tomdamri@gmail.com>
2020-04-05 12:17:11 +0300

    added - consts

---
+++ client/constants/text.js
@@ -0,0 +1,23 @@
+export const _applicationName = "CureOna";
+export const _login = "Login";
+export const _registration = "Registration";
+export const _username = "Username";
+export const _password = "Password";
+export default {
+    applicationName : "CureOna",
+    login : "Login",
+    register : "Register",
+    registration : "Registration",
+    username : "Username",
+    password : "Password",
+    passwordAuthentication : "Password Authentication",
+    placeholder :{
+        username : "The username consists of A-Z, a-z letters",
+        password : "Password must be between 6-8 characters",
+        passwordAuthentication : "Enter the password again",
+    },
+
+
+    or_if_you_dont_have_user :"Or if you dont have a user, "
+}
+
