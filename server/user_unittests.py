5b530eb2b387cee8c1e93646e19283c8cc544558
Matan <matanda1@ac.sce.ac.il>
2020-04-06 00:38:22 +0300

    unittest for customer login

--- server/user_unittests.py
+++ server/user_unittests.py
@@ -4,9 +4,27 @@ from server.user import *
 
 
 class TestUser(unittest.TestCase):
-#register with user name that already exist
+    # register with user name that already exist
     def test_Registration(self):
         url = 'https://cureona.herokuapp.com/Registration'
         myobj = {'username': 'tal', 'password': '123', 'type': 'customer'}
         response = requests.post(url, data=myobj)
         self.assertEqual(response.json(), {'state': 'user name already exist'})
+
+    def test_Login_success(self):
+        url = 'https://cureona.herokuapp.com/Login'
+        myobj = {'username': 'tal', 'password': '123'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'success', 'type': 'admin'})
+
+    def testLoginFailedWrongPassword(self):
+        url = 'https://cureona.herokuapp.com/Login'
+        myobj = {'username': 'tal', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_Failed_user_not_exist(self):
+        url = 'https://cureona.herokuapp.com/Login'
+        myobj = {'username': 'asdfasdf', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
