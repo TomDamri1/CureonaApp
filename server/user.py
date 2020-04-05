b558e993302a60764478b33dbbc4762ea342c001
Matan <matanda1@ac.sce.ac.il>
2020-04-05 19:21:43 +0300

    Login - check if user exist and enter the right password

--- server/user.py
+++ server/user.py
@@ -2,8 +2,20 @@ from flask import jsonify
 from flask_restful import reqparse, Resource
 
 from server.mongo_connection import *
+
 new_col = new_db["login"]
+
+Login_parser = reqparse.RequestParser()
+Login_parser.add_argument('username')
+Login_parser.add_argument('password')
+
 
 class Login(Resource):
     def post(self):
-        pass
+        data = Login_parser.parse_args()
+        print(data)
+        json_doc = new_col.find_one({"user_name": data['username']})
+        if data.password == json_doc['password']:
+            return jsonify({'state': 'success', 'type': json_doc['type']})
+        else:
+            return jsonify({'state': 'failed'})
