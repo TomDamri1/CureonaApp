bc5e0e2d935c362ba01b743c72cc2d814a9267bd
tom <tomdamri@gmail.com>
2020-04-08 16:24:50 +0300

    finished - frontend - make an appointment, wating for server side to finish this story.

--- client/components/SingleHour.js
+++ client/components/SingleHour.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Alert } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import Colors from '../constants/Colors';
+
+const registerToHour = (hour) => {
+    const checkTheHourWithTheServer = true ; // for now its true.
+    if(checkTheHourWithTheServer === true) {
+        //here notify the server for this user registration to the hour
+        //push navigtaion - loading
+        //pop navigation
+        Alert.alert("Sucsses!" ,`you are now registerd to ${hour}. please dont be late.`  )
+    }
+}
 
 
 const SingleHour = props => {
     return (
-        <TouchableOpacity onPress={()=>{}}>
+        <TouchableOpacity onPress={()=>registerToHour(props.time)}>
             <View style={styles.container}>
                 <Text style={styles.title}>
                     {props.time}
