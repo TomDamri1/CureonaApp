4ea6b6b0a168405432a0bad19af49498da3ba460
MichalTshobruzki <michal.tsho@gmail.com>
2020-04-10 14:30:03 +0300

    added- comments

--- client/screens/AdminScreen.js
+++ client/screens/AdminScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import Title from '../components/Title'
 
-const UserScreen = props => {
+const AdminScreen = props => {
     return(
         <View>
             <Title title="welcome!" subTitle={props.navigation.getParam('username')}/>
