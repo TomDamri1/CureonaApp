4074a0d5e58f2fcd3f2015be54143903c432b8bf
MichalTshobruzki <michal.tsho@gmail.com>
2020-04-13 22:21:15 +0300

    added- can navigate to list of all business from admin home page

--- client/screens/AdminScreen.js
+++ client/screens/AdminScreen.js
@@ -8,15 +8,13 @@ const AdminScreen = props => {
         <View>
             <Title title="welcome!" subTitle={props.navigation.getParam('username')}/>
             <Title title="this is an admin screen"/>
-            <Title title="with alot of fancy options"/>
-            <Button title="try" onPress={()=>{props.navigation.navigate({
+            <Button title="View all businesses" onPress={()=>{props.navigation.navigate({
                 routeName : "CustomerSearchScreen",
                 params : {
                     USERTYPE : text.type.admin
                 }
             })}}/>
         </View>
-        
     )
 }
 
