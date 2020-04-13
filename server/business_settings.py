50c8d00a3ebf5d3fc1cc02f21646deacf5d022d4
alfital2 <alfital2@gmail.com>
2020-04-14 00:53:00 +0300

    changed the time format from 8 to 08:00 and so on....

--- server/business_settings.py
+++ server/business_settings.py
@@ -1,8 +1,6 @@
 from flask import jsonify
 from flask_restful import reqparse, Resource
 from server.mongo_connection import *
-import random
-import string
 
 user_queue = new_db["user_queue"]
 business_info = new_db["business_info"]
@@ -16,7 +14,7 @@ updateSettings_parser = reqparse.RequestParser()
 
 updateSettings_parser.add_argument('company_id', required=True, help="company id cannot be blank!")
 updateSettings_parser.add_argument('open', required=True, help="open cannot be blank!")
-updateSettings_parser.add_argument('opening_houres', required=True, help="open_hours cannot be blank!")
+updateSettings_parser.add_argument('open_hours', required=True, help="opening hours cannot be blank!")
 
 
 class updateSettings(Resource):
@@ -27,4 +25,15 @@ class updateSettings(Resource):
 
         json_doc = business_settings.find_one({"company_id": data['company_id']})
 
-        #if json_doc:
+        if json_doc:
+            business_open = data['open']
+            open_hours = data['opening_hours']
+
+            #if valid:
+            business_settings.update({'company_id': data['company_id']},
+                                     {"$set":{"open": business_open ,
+                                              "open_hours" :open_hours}})
+
+            print(business_open)
+            print(open_hours)
+
