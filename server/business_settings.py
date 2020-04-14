09649ee11519a0eff986296437dc7d850b6dc901
alfital2 <alfital2@gmail.com>
2020-04-14 03:59:30 +0300

    now it is possible to update the maximum capacity for business

--- server/business_settings.py
+++ server/business_settings.py
@@ -8,32 +8,56 @@ login = new_db["login"]
 
 business_settings = new_db["business_info"]
 
-
-
 updateSettings_parser = reqparse.RequestParser()
 
 updateSettings_parser.add_argument('company_id', required=True, help="company id cannot be blank!")
-updateSettings_parser.add_argument('open', required=True, help="open cannot be blank!")
-updateSettings_parser.add_argument('open_hours', required=True, help="opening hours cannot be blank!")
+updateSettings_parser.add_argument('open', required=False)
+updateSettings_parser.add_argument('open_hours', type=dict, required=False)
+updateSettings_parser.add_argument('max_capacity', type=int, required=False)
 
 
 class updateSettings(Resource):
 
     def post(self):
-
         data = updateSettings_parser.parse_args()
-
         json_doc = business_settings.find_one({"company_id": data['company_id']})
 
         if json_doc:
-            business_open = data['open']
-            open_hours = data['opening_hours']
+            cid = data['company_id']
+            ret = {}
 
-            #if valid:
-            business_settings.update({'company_id': data['company_id']},
-                                     {"$set":{"open": business_open ,
-                                              "open_hours" :open_hours}})
+            if data['open_hours'] is not None:
+                ret["open_hours"] = "updated"
+                self.updateHours(cid, data['open_hours'])
+            if data['open'] is not None:
+                ret["open"] = "updated"
+                self.updateOpen(cid, True if data['open'] == "True" else False)
+            if data['max_capacity'] is not None:
+                ret["max_capacity"] = "updated"
+                self.updateMaxCapacity(cid, data['max_capacity'])
 
-            print(business_open)
-            print(open_hours)
+            return jsonify({'state': 'success', "changes": 'No chacnges'} if not ret else ret)
 
+        return jsonify({'state': 'company id was not found'})
+
+    def updateMaxCapacity(self, cid, maxCapacity):
+        business_settings.update({'company_id': cid},
+                                 {"$set": {"max_capacity": maxCapacity}})
+
+    def updateOpen(self, cid, business_open):
+        business_settings.update({'company_id': cid},
+                                 {"$set": {"open": business_open}})
+
+    def updateHours(self, cid, open_hours):
+        business_settings.update({'company_id': cid},
+                                 {"$set": {
+                                     "open_hours.sunday": open_hours['sunday'],
+                                     "open_hours.monday": open_hours['monday'],
+                                     "open_hours.tuesday": open_hours['tuesday'],
+                                     "open_hours.wednesday": open_hours['wednesday'],
+                                     "open_hours.thursday": open_hours['thursday'],
+                                     "open_hours.friday": open_hours['friday'],
+                                     "open_hours.saturday": open_hours['saturday'],
+                                 }
+                                 }
+                                 )
