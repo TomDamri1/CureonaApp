a0d40f21a978d87b1949067d428d719837432965
Tom <44465079+TomDamri1@users.noreply.github.com>
2020-04-29 20:36:02 +0300

    Update test_system.py

--- tests/test_system.py
+++ tests/test_system.py
@@ -6,7 +6,7 @@ import requests
 class TestUser(unittest.TestCase):
     # register with user name that already exist
     def test_Registration(self):
-        url = 'https://cureona.herokuapp.com/Registration'
+        url = 'https://curona.herokuapp.com/Registration'
         myobj = {'username': 'tal', 'password': '123', 'type': 'customer'}
         response = requests.post(url, data=myobj)
         self.assertEqual(response.json(), {'state': 'user name already exist'})
