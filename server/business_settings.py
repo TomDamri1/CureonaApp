9d1fba88d094c39135047e039d7e7642ab487f0e
alfital2 <alfital2@gmail.com>
2020-04-30 03:26:18 +0300

    reformatting the code

--- server/business_settings.py
+++ server/business_settings.py
@@ -120,8 +120,6 @@ def update_hours_and_inform_costumers(cid, new_opening_hours, costumers_affected
                 if len(tmp_queue) != 0:
                     costumers_affected[day] = tmp_queue
 
-
-
     business_settings.update({'company_id': cid},
                              {"$set": {"queue": new_opening_hours_queue,
                                        "open_hours": new_opening_hours
