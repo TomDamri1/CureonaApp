886049dda275b5da8ca362d32aa1f9649e27c728
alfital2 <alfital2@gmail.com>
2020-05-01 01:00:21 +0300

    separating unit tests files into folders

---
+++ tests/test_login.py
@@ -0,0 +1,70 @@
+import unittest
+import requests
+
+
+class TestUserLogin(unittest.TestCase):
+
+    def test_Login_success(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'c_test', 'password': '123'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'success', 'type': 'customer'})
+
+    def testLoginFailedWrongPassword(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'c', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_Failed_user_not_exist(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'asdfasdf', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+# ---------------------------------------------------------------------
+# business owner
+
+    def test_Login_Failed_business_owner_not_exist(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'asdfasdf', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_Failed_business_owner_wrong_password(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'bo', 'password': '12344'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_success_business_owner(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'test', 'password': '123'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'success', "company_id": "123", 'type': 'business_owner'})
+
+
+
+    # ---------------------------------------------------------------------
+    # admin login checks
+    def test_Login_Failed_admin_not_exist(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'asdfasdf', 'password': '1234'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_Failed_admin_wrong_password(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'admin', 'password': '12344'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'failed'})
+
+    def test_Login_success_admin(self):
+        url = 'https://curona.herokuapp.com/Login'
+        myobj = {'username': 'admin', 'password': '123456'}
+        response = requests.post(url, data=myobj)
+        self.assertEqual(response.json(), {'state': 'success', 'type': 'admin'})
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_queue_settings.py
@@ -0,0 +1,42 @@
+import unittest
+import requests
+
+
+class TestUserUpdateSettings(unittest.TestCase):
+
+    def test_get_two_queue_to_same_place_at_same_time(self):
+        url = 'https://curona.herokuapp.com/businessSettings'
+        myobj = {'company_id': '123', 'open': 'True'}
+        requests.post(url, data=myobj)
+        url = 'https://curona.herokuapp.com/GetQueue'
+        myobj = {"username": "c_test", 'company_id': '123', "BusinessName": "IKEA", "Day": "wednesday", "Hour": "15:00"}
+        requests.post(url, data=myobj)
+        response = requests.post(url, data=myobj)
+        except_result = {"state": "success, sorry you can not get two queue to the same hour"}
+        self.assertEqual(response.json(), except_result)
+
+    def test_get_queue_to_closed_business(self):
+        url = 'https://curona.herokuapp.com/businessSettings'
+        myobj = {'company_id': '123', 'open': 'False'}
+        requests.post(url, data=myobj)
+        url = 'https://curona.herokuapp.com/GetQueue'
+        myobj = {"username": "c_test", 'company_id': '123', "BusinessName": "IKEA", "Day": "wednesday",
+                 "Hour": "15:00-16:00"}
+        response = requests.post(url, data=myobj)
+        except_result = {"state": "success, Business is closed"}
+        myobj = {'company_id': '123', 'open': 'True'}
+        requests.post(url, data=myobj)
+        self.assertEqual(response.json(), except_result)
+
+    def test_get_my_queue(self):
+        url = 'https://curona.herokuapp.com/GetMyQueue'
+        myobj = {"username": "IsNotExist"}
+        response = requests.post(url, data=myobj)
+        except_result = list()
+        self.assertNotEqual(response, except_result)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
