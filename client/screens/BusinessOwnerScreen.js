5cf9fd4b4b031fe33dc93e50c8c1e7ffd63a5f12
MichalTshobruzki <michal.tsho@gmail.com>
2020-05-01 14:48:43 +0300

    added- connection to the add worker screen

--- client/screens/BusinessOwnerScreen.js
+++ client/screens/BusinessOwnerScreen.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import Title from '../components/Title'
 
 const BusinessOwnerScreen = props => {
+    const company_id = props.navigation.getParam('company_id');
     return(
         <View>
             <Title title="welcome!" subTitle={props.navigation.getParam('username')}/>
             <Title title={`your business : ${props.navigation.getParam('businessName')}`}/>
+            <Button title="Add worker user to my business" onPress={() => {
+                props.navigation.setParams({company_id : company_id});
+                props.navigation.navigate({
+                    routeName: "AddWorkerScreen",
+                    params : {
+                        company_id : company_id
+                    }
+                })
+            }} />
         </View>
 
     )
