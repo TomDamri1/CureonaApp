a76dba6990b2de14dd03cb542b0820993df255eb
TomDamri1 <tomdamri@gmail.com>
2020-05-03 17:38:16 +0300

    fixed - CustomerQueuesScreen bug

--- client/navigation/LoginNavigator.js
+++ client/navigation/LoginNavigator.js
@@ -12,44 +12,42 @@ import CustomerScreen from "../screens/CustomerScreen";
 import BusinessOwnerScreen from "../screens/BusinessOwnerScreen";
 import AdminScreen from '../screens/AdminScreen';
 import LoadingScreen from '../screens/LoadingScreen';
-import CustomerSearchScreen from '../screens/CustomerSearchScreen'
+import CustomerSearchScreen from '../screens/CustomerSearchScreen';
+import CustomerQueuesScreen from '../screens/CustomerQueuesScreen'
 import AppointmentScreen from '../screens/AppointmentScreen';
 import AdminChangesScreen from '../screens/AdminChangesScreen';
-<<<<<<< HEAD
 import WorkerScreen from '../screens/WorkerScreen';
 import AddWorkerScreen from '../screens/AddWorkerScreen';
-=======
-import CustomerQueuesScreen from '../screens/CustomerQueuesScreen';
->>>>>>> 678385523ae3744ba95453409176e37cf86551b5
 
 const LoginNavigator = createStackNavigator({
     Login: LoginScreen,
     Registration: RegisterScreen,
     CustomerScreen: CustomerScreen,
-    CustomerSearchScreen : {
-        screen : CustomerSearchScreen,
-        navigationOptions : {
-            headerTitle : text.screenHeaders.search
+    CustomerSearchScreen: {
+        screen: CustomerSearchScreen,
+        navigationOptions: {
+            headerTitle: text.screenHeaders.search
         }
     },
-    AppointmentScreen : AppointmentScreen,
+    AppointmentScreen: AppointmentScreen,
     BusinessOwnerScreen: BusinessOwnerScreen,
     AdminScreen: AdminScreen,
-    Loading : {
-        screen :LoadingScreen,
-        navigationOptions:{
-            headerShown : false
+    Loading: {
+        screen: LoadingScreen,
+        navigationOptions: {
+            headerShown: false
         }
     },
-    AdminChangesScreen : AdminChangesScreen,
+    AdminChangesScreen: AdminChangesScreen,
     WorkerScreen: WorkerScreen,
-    Loading : {
-        screen :LoadingScreen,
-        navigationOptions:{
-            headerShown : false
+    Loading: {
+        screen: LoadingScreen,
+        navigationOptions: {
+            headerShown: false
         }
     },
-    AddWorkerScreen : AddWorkerScreen,
+    AddWorkerScreen: AddWorkerScreen,
+    CustomerQueuesScreen: CustomerQueuesScreen,
 })
 
 
