72cb59d8cd5bf4ac9dbb763a3a5adc7bbbb0db2e
MichalTshobruzki <michal.tsho@gmail.com>
2020-05-06 20:58:01 +0300

    added- BusinessOwnerDayPicker for choosing the day to change

---
+++ client/components/BusinessOwnerDayPicker.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, Text, View, Picker } from 'react-native';
+import Colors from '../constants/Colors';
+import Time from '../constants/Time';
+
+const NUMBER_OF_DAYS_IN_THE_WEEK = 7
+
+
+
+const BusinessOwnerDayPicker = props => {
+    //const [selectedDayValue, setSelectedDayValue] = props.selectedDayValueState;
+    
+    //const now = new Date();
+    //const daynum = now.getDay();
+    let dayList = [];
+    for (let index = 0; index < NUMBER_OF_DAYS_IN_THE_WEEK; index++) {
+
+        const thisDay = Time.days[index];
+        dayList.push(
+            <Picker.Item label={`${thisDay}`} value={thisDay} key={thisDay} />
+        );
+    }
+    dayList.push(
+        <Picker.Item label={`${'All Week'}`} value={'All Week'} key={'All Week'} />
+    );
+    return (
+
+        <View>
+
+            <Picker
+                itemStyle={{ borderBottomColor: Colors.primaryColor, borderBottomWidth: 1 }}
+            >
+                {
+                     dayList
+                }
+            </Picker>
+        </View>
+
+    )
+}
+
+export default BusinessOwnerDayPicker
+
+const styles = StyleSheet.create({})
