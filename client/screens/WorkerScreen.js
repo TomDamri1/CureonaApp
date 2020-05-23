11ff51aea28d6a4bcb443f71d9f2a67d76e52ae7
TomDamri1 <tomdamri@gmail.com>
2020-05-24 02:13:50 +0300

    added - SpontaneousAppointment

--- client/screens/WorkerScreen.js
+++ client/screens/WorkerScreen.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, Alert } from 'react-native';
 import Title from '../components/Title';
 import text from '../constants/text';
 import Colors from '../constants/Colors';
 import Urls from '../constants/Urls';
 import Response from '../constants/Response';
+import requestFromUrl from '../functions/routeFunctions/requestFromUrl';
 
 //stxjwi - worker name
+
+
+const checkAmountOfPeaple = async (company_id) => {
+    const req = await requestFromUrl(Urls.routes.currentAmountOfPeapleInTheStore, { company_id: company_id });
+    const returnValue = await req.current_amount_in_business;
+    return returnValue;
+}
+
 
 const WorkerScreen = props => {
     const [entranceKey, setEntranceKey] = useState('');
     const [pass, setPass] = useState('#fff');
     const [canTheUserGetIn, setCanTheUserGetIn] = useState(false);
+    const [amountOfCustomersInBusiness, setAmountOfCustomersInBusiness] = useState('0');
     const username = props.navigation.getParam('username');
     const company_id = props.navigation.getParam('company_id');
     console.log(company_id)
+
+    const handleSpontaneousAppointment = async () => {
+        
+        const resData = await requestFromUrl(Urls.routes.SpontaneousAppointment, {"company_id": company_id,"cellphone" : entranceKey}) ;
+        if (resData.state === "success"){
+            Alert.alert("Success");
+        }
+        else {
+            Alert.alert("Falied");
+            console.log(resData);
+        }
+    }
 
     const handleInPress = async () => {
         props.navigation.navigate({
@@ -57,6 +79,14 @@ const WorkerScreen = props => {
         }
 
     }
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            const AmountOfPeaple = await checkAmountOfPeaple(company_id);
+            setAmountOfCustomersInBusiness(AmountOfPeaple);
+        }, 5000);
+        return () => clearInterval(interval);
+    })
+
     const handleOutPress = async () => {
 
     }
@@ -88,8 +118,18 @@ const WorkerScreen = props => {
                     />
                 </View>
             </View>
+            <View>
+                <Button
+                    color={Colors.primaryColor}
+                    title ="Spontaneous Appointment"
+                    onPress = {() => handleSpontaneousAppointment()}
+                />
+            </View>
             <View style={{ ...styles.getinTextContainer, backgroundColor: pass }}>
                 <Text style={styles.getinText}>{canTheUserGetIn ? text.theUserCanGetIn : text.theUserCannotGetIn}</Text>
+            </View>
+            <View>
+                <Text>Number of peaple in the shop : {amountOfCustomersInBusiness}</Text>
             </View>
         </View>
     )
@@ -118,17 +158,17 @@ const styles = StyleSheet.create({
         margin: "5%",
         width: "40%"
     },
-    getinText : {
-        fontSize :25,
-        color : 'white',
-        letterSpacing:1.2
+    getinText: {
+        fontSize: 25,
+        color: 'white',
+        letterSpacing: 1.2
     },
-    getinTextContainer : {
-        paddingTop:5,
-        width: '100%', 
+    getinTextContainer: {
+        paddingTop: 5,
+        width: '100%',
         height: 50,
-        textAlign:"center",
-        alignItems:"center"
+        textAlign: "center",
+        alignItems: "center"
     }
 
 })
