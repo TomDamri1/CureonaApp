435d7f5acc62ec056ce4a17127cc0da3a39b4afc
MatanDavidian <matanda1@ac.sce.ac.il>
2020-06-08 18:03:47 +0300

    update requirement file

--- tests/measure_url_response.py
+++ tests/measure_url_response.py
@@ -8,3 +8,5 @@ class TestURLs(unittest.TestCase):
     def test_Change_amount_of_people_in_a_business(self):
         url = 'https://curona.herokuapp.com/MeasureUrlsResponse'
         requests.post(url)
+        print(123)
+
